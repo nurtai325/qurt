41f638768e70714566ec68b735dcdd036bbed131
nurtai325 <nurtolymbek23@gmail.com>
2025-05-05 14:58:12 +0500

    include header to editor

--- editor/src/index.js
+++ editor/src/index.js
@@ -1,7 +1,21 @@
 import { createRoot } from 'react-dom/client';
+import { App } from './App';
 import "./index.css";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
-	<div className='text-blue-600'>hello</div>
+	<>
+		<header className="flex justify-between items-center px-10 py-2 bg-white shadow-md w-full">
+			<a href="/">
+				<div className="flex items-center">
+					<img src="/assets/images/logo.png"
+						className="w-[35px] h-[35px] bg-[#ff9c1a] rounded-[5px] mr-[10px]" />
+					<span className="font-bold text-[20px] text-[#575e75]">Qurt Қазақша</span>
+				</div>
+			</a>
+		</header>
+		<div>
+			<App />
+		</div>
+	</>
 );
