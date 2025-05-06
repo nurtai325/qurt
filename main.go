23c8ffc9c3b9bc3cfc65f2ff507cf9e531890e0f
nurtai325 <nurtolymbek23@gmail.com>
2025-05-06 23:28:22 +0500

    github webhook init

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
 
+func main() {
+	secret := os.Getenv("GIT_SECRET")
+	if secret == "" {
+		panic("secret is empty. set GIT_SECRET env variable")
+	}
+	http.HandleFunc("/githook", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("hello")
+		fmt.Println(r.Header)
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(body))
+	})
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
 }
