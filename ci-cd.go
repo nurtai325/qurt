00bb16f4c22af2f1f66f6cf422b25d0358bf20da
nurtai325 <nurtolymbek23@gmail.com>
2025-05-06 23:43:05 +0500

    change readme

--- ci-cd.go
+++ ci-cd.go
@@ -12,16 +12,16 @@ func main() {
 	if secret == "" {
 		panic("secret is empty. set GIT_SECRET env variable")
 	}
-	http.HandleFunc("POST /githook/", func(w http.ResponseWriter, r *http.Request) {
+	err := http.ListenAndServe(":8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("hello")
+		fmt.Println(r.URL.Path)
 		fmt.Println(r.Header)
 		body, err := io.ReadAll(r.Body)
 		if err != nil {
 			panic(err)
 		}
 		fmt.Println(string(body))
-	})
-	err := http.ListenAndServe(":8080", nil)
+	}))
 	if err != nil {
 		panic(err)
 	}
